85dc5c7613a0909aac6c29eca1edd78d0cb04a9f
msegovia-unlam <msegoviacalderon@alumno.unlam.edu.ar>
2021-07-03 02:50:42 -0300

    Correcciones esteticas de homeadmin

--- view/homeAdminView.php
+++ view/homeAdminView.php
@@ -1,18 +1,17 @@
 {{>headerLogeado}}
+<div style="height: 80vh; width:auto;">
+    <h3>ESTAS LOGEADO</h3>
+    <div class="row p-5">
+        <div class="container-md my-5 border col-sm-4">
+            <h2 class="my-3">Proformas</h2>
+            <a href="/proforma"><button class="btn btn-dark my-3">Llenar Proforma</button></a>
+            <a href="/homeadmin/verproformas"><button class="btn btn-dark my-3">Proformas Registradas</button></a>
+        </div>
 
-<h3>ESTAS LOGEADO</h3>
-
-<div class="row p-5">
-    <div class="container-md my-5 border col-sm-4">
-        <h2 class="my-3">Proformas</h2>
-        <a href="/proforma"><button class="btn btn-dark my-3">Llenar Proforma</button></a>
-        <a href="/homeadmin/verproformas"><button class="btn btn-dark my-3">Proformas Registradas</button></a>
-    </div>
-
-    <div class="container-md my-5 border col-sm-4">
-        <h2 class="my-3">Empleados</h2>
-        <a href="/homeadmin/verempleados"><button class="btn btn-dark my-3">Ver Empleados</button></a>
+        <div class="container-md my-5 border col-sm-4">
+            <h2 class="my-3">Empleados</h2>
+            <a href="/homeadmin/verempleados"><button class="btn btn-dark my-3">Ver Empleados</button></a>
+        </div>
     </div>
 </div>
-
 {{>footer}}
