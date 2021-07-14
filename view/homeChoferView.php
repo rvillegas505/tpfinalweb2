4c18ede82e4811a6933bdb12c78f0a75a2de770c
msegovia-unlam <msegoviacalderon@alumno.unlam.edu.ar>
2021-07-14 20:47:17 -0300

    se corrige esteticamente home chofer

--- view/homeChoferView.php
+++ view/homeChoferView.php
@@ -1,9 +1,13 @@
 {{>headerLogeado}}
 
-<h3>ESTAS LOGEADO, SOS CHOFER</h3>
-
-
-<a href="/homeChofer/mostrarRegistroPosicion"><button class="btn btn-dark my-3">Registrar datos del viaje</button></a>
-
+<div style="height: 80vh; width:auto;">
+    <h3>ESTAS LOGEADO, SOS CHOFER</h3>
+    <div class="row p-5">
+        <div class="container-md my-5 border col-sm-4">
+            <h2 class="my-3">Datos del viaje</h2>           
+            <a href="/homeChofer/mostrarRegistroPosicion"><button class="btn btn-dark my-3">Registrar datos del viaje</button></a>
+        </div>        
+    </div>
+</div>
 
 {{>footer}}
