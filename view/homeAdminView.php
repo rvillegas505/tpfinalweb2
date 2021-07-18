90e569a5012bdb0a8772e8327e268cf6dd53fb10
bdellavecchia <bdellavecchia@alumno.unlam.edu.ar>
2021-07-17 23:42:03 -0300

    logo

--- view/homeAdminView.php
+++ view/homeAdminView.php
@@ -1,6 +1,5 @@
 {{>headerLogeado}}
 <div style="height: 80vh; width:auto;">
-    <h3>ESTAS LOGEADO</h3>
     <div class="row p-5">
         <div class="container-md my-5 border col-sm-4">
             <h2 class="my-3">Proformas</h2>
--- view/homeChoferView.php
+++ view/homeChoferView.php
@@ -1,7 +1,6 @@
 {{>headerLogeado}}
 
 <div style="height: 80vh; width:auto;">
-    <h3>ESTAS LOGEADO, SOS CHOFER</h3>
     <div class="row p-5">
         <div class="container-md my-5 border col-sm-4">
             <h2 class="my-3">Datos del viaje</h2>           
